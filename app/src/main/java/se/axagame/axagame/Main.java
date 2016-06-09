4d68424b9230d1fb486aae033c9a2d4de038698f
T4vxo <daniel.lundberg@utb.vxo.se>
2016-06-09 09:50:24 +0200

    Added gameloop

--- app/src/main/java/se/axagame/axagame/Main.java
+++ app/src/main/java/se/axagame/axagame/Main.java
@@ -1,13 +1,24 @@
 package se.axagame.axagame;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class Main extends AppCompatActivity {
+public class Main extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //turn of title
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //full screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+
+        setContentView(new GamePanel(getBaseContext()));
     }
 }
